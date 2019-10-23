7404782d38bb00590ea88c60fb23db58d7e06d4e
Mayank Sharma <mayank8019@gmail.com>
2019-10-23 17:36:29 +0530

    Code for insights into the hardware-prefetcher

--- page-size-test.c
+++ page-size-test.c
@@ -1,31 +1,46 @@
 #include "util.h"
 
-// 16 MB of memory allocated
-const long long MEM_TO_ALLOC =  128 << 20;
+// 128 MB of memory allocated
+#define STRIDE_LENGTH 512LL
+
+const long long MEM_TO_ALLOC =  16 << 20;
 const long long HUGE_PAGE_SIZE =  2 << 20;
 const long long NORMAL_PAGE_SIZE =  4 << 10;
+const long long N_ACCESSES = MEM_TO_ALLOC / STRIDE_LENGTH;
 
 int main () {
     char *data = malloc (sizeof (char) * MEM_TO_ALLOC);
-    CYCLES read_cycles[MEM_TO_ALLOC / NORMAL_PAGE_SIZE];
+    int rand_access[N_ACCESSES];
+
+    CYCLES read_cycles[N_ACCESSES];
     CYCLES out_start, cycles_taken;
 
     memset (data, '0', MEM_TO_ALLOC);
 
+    for (int i = 0; i < N_ACCESSES; i++)
+        rand_access[i] = rand() % (N_ACCESSES);
+
     out_start = rdtscp ();
-    for (int i = 0; i < MEM_TO_ALLOC / NORMAL_PAGE_SIZE; i++) {
+    for (int i = 0; i < N_ACCESSES; i++) {
         CYCLES start;
 
         start = rdtscp ();
-        volatile char x = data[i];
-
-        /* read_cycles[i] = rdtscp () - start; */
-        /* volatile char y = x - (i % '0'); */
+        volatile char x = data[rand_access[i] * STRIDE_LENGTH];
     }
+
+    /* printf ("Mem to alloc = %lld\n", MEM_TO_ALLOC); */
     cycles_taken = rdtscp () - out_start;
 
-    /* for (int i = 0; i < MEM_TO_ALLOC / NORMAL_PAGE_SIZE; i++) {
-     *     printf ("[%d]\t %lu\n", i, read_cycles[i]);
-     * } */
+    for (int i = 0; i < N_ACCESSES; i++) {
+        CYCLES start;
+        start = rdtscp ();
+        volatile char x = data[rand_access[i] * STRIDE_LENGTH];
+
+        read_cycles[i] = rdtscp () - start;
+    }
+
+    for (int i = 0; i < N_ACCESSES; i++) {
+        printf ("[%lld]\t %lu\n", rand_access[i] * STRIDE_LENGTH, read_cycles[i]);
+    }
     printf ("Cycles Taken = %lu\n", cycles_taken);
 }
